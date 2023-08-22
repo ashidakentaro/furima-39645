6eef87ee2e68347f4deda1d007e8adafc028e624
ashidakentaro <ashikenpawg7293@gmail.com>
2023-08-22 20:03:17 +0900

    商品情報編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,8 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
-  
+  before_action :authenticate_user!, except: [:index, :show,]
+  before_action :set_item, only: [:edit, :update]
+  before_action :contributor_confirmation, only: [:edit, :update]
+
   def index
     @items = Item.order("created_at DESC")
   end
@@ -23,10 +25,26 @@ class ItemsController < ApplicationController
     @user = @item.user
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
 private
 
   def item_params
     params.require(:item).permit(:image,:title,:explanation,:price,:category_id,:condition_id,:charge_id,:prefecture_id,:leadtime_id).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
   def contributor_confirmation
